5ed6d7b662492be43ac95a82f60710ccb0fc476d
Your Name <you@example.com>
2025-07-19 18:41:50 -0400

    update

--- app/llm.py
+++ app/llm.py
@@ -1,8 +1,8 @@
 from groq import Groq
 import streamlit as st
 
-# api_key =  st.secrets["API_KEY"]
-client = Groq(api_key='api_key')
+api_key =  st.secrets["API_KEY"]
+client = Groq(api_key=api_key)
 
 
 system_prompt = '''You are an expert summarizer helping a user prepare input for a sentence embedding model with a strict 512-token input limit.
